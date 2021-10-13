d2d5e2d9594ce0ec29e443a3d8efdb6c71445a71
vishalj1412 <vishaljagtap753@gmail.com>
2021-10-13 09:54:06 +0530

    Add: Queue oprations using linkedlist

---
+++ QueueTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class QueueTest {
+    @Test
+    public void QTest()
+    {
+        Queue<Integer> obj=new Queue<>();
+        obj.enQueue(56);
+        obj.enQueue(30);
+        obj.enQueue(70);
+
+        obj.DisplayQueue();
+        System.out.println("");
+        System.out.println("Size of Queue "+obj.Size());
+        System.out.println("Deleted element is "+obj.deQueue());
+        System.out.println("Size of Queue after delete element "+obj.Size());
+        System.out.println("Queue after delete element");
+        obj.DisplayQueue();
+    }
+}
